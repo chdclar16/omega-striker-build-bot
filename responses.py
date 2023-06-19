7924b8daf7c3efa5bd2781144de35835af0c857e
Chad Manuel <chadc_manuel@yahoo.com>
2023-06-19 12:22:13 -0700

    Working build of Omega Striker Build Bot

---
+++ responses.py
@@ -0,0 +1,300 @@
+def get_response(message: str):
+    p_message = message.lower()
+
+    if p_message == 'juliette':
+        return ['https://steamuserimages-a.akamaihd.net/ugc/2019347876656142426/0B11876F2A0577EBB459D96B52522DBF5FC5FDC0/', None,
+                """
+                Good Awakening Combinations
+                - **ONE-TWO PUNCH and HEAVY IMPACT**
+                Enhances your secondary-primary combo. Easier to get KOs.
+
+                - **ONE-TWO PUNCH and TEMPO SWING**
+                Increases your damage.
+
+                - **PERFECT FORM and STINGER**
+                A deadly combination that could KO/make anyone staggered with time.
+                """
+                , None
+                ]
+    elif p_message == 'kai':
+        return ['https://steamuserimages-a.akamaihd.net/ugc/2019347876656139649/AEA58AB0AE1F111858E5453F7D8477464BB49816/', 'https://steamuserimages-a.akamaihd.net/ugc/2019347876656139640/67941A443F6B38E18D7E4D19D1EF9FAADB3C60C4/',
+                """
+                Good Awakening Combinations
+                - **AERIALS and PRIMETIME**
+                Upgrade your haste and slightly longer 2x Primary
+
+                - **TWIN DRIVE and CAST TO LAST**
+                2x Secondary that last longer
+
+                - **PRIMETIME and MISSILE PROPULSION**
+                Way further 2x Primary
+                """,
+                """
+                Good Awakening Combinations
+                - **PRIMETIME and MISSILE PROPULSION**
+                Way further 2x Primary, making the Core way further to the enemy side of the field
+
+                - **PRIMETIME and HOTSHOT**
+                2x Primary that reduces the cooldown every time it hits the Core
+
+                - **CHRONOBOOST and TWIN DRIVE**
+                2x faster speed secondary
+                """
+                ]
+    elif p_message == 'dubu':
+        return [None, 'https://steamuserimages-a.akamaihd.net/ugc/2019347876656139634/C3587C657A546C8E1023714995E8AC89D326C57A/', None,
+                """
+                Good Awakening Combinations
+                - **BIG FISH and BUILT DIFFERENT**
+                Makes your size and strike zone bigger
+
+                - **TWIN DRIVE and CAST TO LAST**
+                2x Secondary and longer stun if it hits the enemy
+
+                - **BIG FISH and STACKS ON STACKS**
+                Bigger size + Speed
+                """]
+    elif p_message == 'estelle':
+        return ['https://steamuserimages-a.akamaihd.net/ugc/2019347876656139621/182D310C5AEB87E0A21F41D80E4421BFF8BD5328/', "https://steamuserimages-a.akamaihd.net/ugc/2019347876656139615/D8F9B08762A03BF7A4EC3ACBE8DDC4149188CA95/",
+                """
+                Good Awakening Combinations
+                - **STINGER and PRIMETIME**
+                Turns you into one of the best DPS.
+
+                - **DEADEYE and ONE-TWO PUNCH**
+                Blink (secondary) and Primary enemies towards the wall become a deadly combo with these combinations
+
+                - **SUPER SURGE and TWIN DRIVE**
+                Longer Blinks with 2 Charge, good for scoring a goal against off guarded goalie
+                """,
+                """
+                Good Awakening Combinations
+                - **PRIMETIME and RAPID FIRE**
+                2x Primary and lower cooldown, good for keeping the ball at the other side
+
+                - **PRIMETIME and HOTSHOT**
+                2x Primary and reduced cooldown whenever you hit the Core with an ability
+
+                - **HOTSHOT and TWIN DRIVE**
+                Good for saving cross shots/enemies shot to the other side of the goal
+                """
+                ]
+    elif p_message == 'atlas':
+        return [None, 'https://steamuserimages-a.akamaihd.net/ugc/2019347876656139607/0EA7EDF9D212540BF9E95B758F17BA6CD6F972DA/', None,
+                """
+                Good Awakening Combinations
+                - **TWIN DRIVE and TIMELESS CREATOR**
+                2x Secondary that last longer if bloomed
+
+                - **TWIN DRIVE and HOTSHOT**
+                2x Secondary and reduces every cooldown whenever you hit the Core with an ability
+
+                - **HOTSHOT and PRIMETIME**
+                2x Primary and reduces every cooldown whenever you hit the Core with an ability
+                """ ]
+    elif p_message == 'juno':
+        return ["https://steamuserimages-a.akamaihd.net/ugc/2019347876656144426/B53B120515ECF8EEB64C2C44644436F05DD523E0/", None,
+                """
+                Good Awakening Combinations
+                - **PRIMETIME and HOTSHOT**
+                2x Primary and reduces every cooldown whenever you hit the Core with an ability
+
+                - **MONUMENTALIST and TIMELESS CREATOR**
+                Bigger Blobbos that last longer
+
+                - **STINGER and PRIMETIME**
+                Turns Juno into a brawler
+                """
+                ,
+                None
+                ]
+    elif p_message == "drek'ar":
+        return ["https://steamuserimages-a.akamaihd.net/ugc/2019347876656144404/53D0938810AFAEE1B14CAEF686100084BFCAB5B0/", None,
+                """
+                Good Awakening Combinations
+                - **PRIMETIME and RAPID FIRE**
+                Increase your Primary into 3 charges and reduced cooldown
+
+                - **HEAVY IMPACT and ONE-TWO PUNCH**
+                KO easier for enemies who like to stand at the edge/near the wall
+
+                - **CAST TO LAST and SPECIALIZED TRAINING**
+                Can make anyone Stagger with your Special
+                """
+                ,
+                None
+                ]
+    elif p_message == "rune":
+        return ["https://steamuserimages-a.akamaihd.net/ugc/2019347876656144397/5EFB1C051E58A475898B6C14ED79AEBD775D70D2/", None,
+                """
+                Good Awakening Combinations
+                - **PRIMETIME and TIMELESS CREATOR**
+                Primary with 2 charges that last longer
+
+                - **PRIMETIME and MONUMENTALIST**
+                Bigger Primary with 2 charges
+
+                - **PRIMETIME and HOTSHOT**
+                Primary with 2 charges and reduced cooldown if you hit the Core with an ability
+                """
+                ]
+    elif p_message == 'x':
+        return ["https://steamuserimages-a.akamaihd.net/ugc/2019347876656144392/0542FFAE240B49988F5D53978A3612332E7AF892/", None,
+                """
+                Good Awakening Combinations
+                - **SPECIALIZED TRAINING and CAST TO LAST**
+                X's Special will do more damage and last longer
+
+                - **PRIMETIME and TEMPO SWING**
+                Deal more damage and Primary has 2 charges
+
+                - **SPECIALIZED TRAINING and EXTRA SPECIAL**
+                Enhances your special damage and cooldown
+                """,
+                None
+                ]
+    elif p_message == 'era':
+        return ["https://steamuserimages-a.akamaihd.net/ugc/2019347876656144386/049486FCDBE8967A2E5B3265B29612792F3DCEC1/", None,
+                """
+                Good Awakening Combinations
+                - **CAST TO LAST and PRIMETIME**
+                Era's Buff and Debuff last longer + 2 charges for Primary
+
+                - **PRIMETIME and STINGER**
+                Primary has 2 charges, it's a very good tool for harassing enemies or potentially KO enemy
+
+                - **MONUMENTALIST or AERIALS and MISSILE PROPULSION**
+                Era's Special becomes way bigger and last longer, the enemy must avoid it or they will take a lot of damage from it
+                """,
+                None
+                ]
+    elif p_message == 'luna':
+        return ["https://steamuserimages-a.akamaihd.net/ugc/2019347876656144381/A02BC9D4C7B2815A21461FA0938B48AA60A86FA1/", "https://steamuserimages-a.akamaihd.net/ugc/2019347876656144375/788723D35905031664A67C14177319170CC29CD0/",
+                """
+                Good Awakening Combinations
+                - **SPECIALIZED TRAINING and EXTRA SPECIAL**
+                The enemy needs to avoid your nuke or they will either get KO'd or staggered, if they evade you special then you have successfully wasted their Energy
+
+                - **PRIMETIME and STINGER**
+                Primary has 2 charges and deals a lot of damage over time
+
+                - **TWIN DRIVE and STINGER**
+                Secondary has 2 charges and you can deal an insane amount of damage if you hit the enemy
+                """,
+                """
+                Good Awakening Combinations
+                - **PRIMETIME and MISSILE PROPULSION**
+                Luna's primary is a good tool to either damage the enemies or keep the Core on the other side
+
+                - **BUILT DIFFERENT and BIG FISH**
+                Since Luna doesn't really have a defensive ability, making her strike zone bigger is a good option
+
+                - **SPECIALIZED TRAINING and EXTRA SPECIAL**
+                The enemy needs to avoid your nuke or they will either get KO'd or staggered, if they evade you special then you have successfully wasted their Energy
+                """
+                ]
+    elif p_message == "asher":
+        return ["https://steamuserimages-a.akamaihd.net/ugc/2019347876656144366/D87196BC9A184DFB3BC874395CE4211FE9C6AAE4/", "https://steamuserimages-a.akamaihd.net/ugc/2019347876656145063/05927DD27795D9257556D6F54343810F0458D4F5/",
+                """
+                Good Awakening Combinations
+                - **BUILT DIFFERENT and BIG FISH**
+                Asher's Primary, Secondary, and Passive also gain size which makes it easier to score a goal
+
+                - **PRIMETIME and TWIN DRIVE**
+                Second time's the charm
+
+                """,
+                """
+                Good Awakening Combinations
+                - **PRIMETIME and BUILT DIFFERENT or BUILT DIFFERENT BIG FISH**
+                Asher's Primary gains another charge, which means her passive also gains a charge
+
+                - **PRIMETIME and HOTSHOT**
+                Primary has 2 charges and every cooldown is reduced after hitting the Core with an ability
+
+                - **TWIN DRIVE and BUILT DIFFERENT**
+                2 Secondary and Bigger passive
+                """
+                ]
+    elif p_message == "ai.mi":
+        return ["https://steamuserimages-a.akamaihd.net/ugc/2019347876656145057/7EF32F9A7D7DE2C75A09579C566A64ED43672A69/", "https://steamuserimages-a.akamaihd.net/ugc/2019347876656145050/0745F842A96766CB8E4ED5FE31C126C40273835B/",
+                """
+
+                Good Awakening Combinations
+                - **SUPER SURGE and MISSILE PROPULSION**
+                Her Secondary and Primary go further
+
+                - **AERIALS and TWIN DRIVE**
+                Secondary has 2 charges, Secondary and Primary go further
+
+                - **AERIALS and PRIMETIME**
+                Primary has 2 charges, Primary and Secondary go further
+
+                """,
+                """
+                Good Awakening Combinations
+                - **AERIALS and TWIN DRIVE**
+                Secondary has 2 charges, Secondary and Primary go further. Disturbing enemy forwards Core control with your blink is a good defense
+
+                - **PRIMETIME and MISSILE PROPULSION**
+                Primary has 2 charges and goes further. Disturbing enemy forwards Core control with your Primary is a good defense
+
+                - **AERIALS and SUPER SURGE**
+                Ai.Mi's blink (secondary) goes way further to the point of reaching the enemy goal gates without leaving your own goal, it is a good offense tool if your forwards need help with scoring or breaking a barrier
+                """
+                ]
+    elif p_message == "zentaro":
+        return ["https://steamuserimages-a.akamaihd.net/ugc/2019347876656145041/24E178D48874AB73A91502D21F74B936A1A5BD15/", None,
+                """
+                Good Awakening Combinations
+                - **STINGER and TEMPO SWING**
+                Zentaro's combo deals an insane amount of damage that could make anyone stagger with time
+
+                - **TEMPO SWING and PRIMETIME**
+                Primary has 2 charges and TEMPO SWING increases your damage by an insane amount
+
+                - **TWIN DRIVE and SUPER SURGE**
+                Secondary has 2 charges that deal more damage and goes further
+                """,
+                None
+                ]
+    elif p_message == "rasmus":
+        return ["https://steamuserimages-a.akamaihd.net/ugc/2019347876656145034/848CB0DEADDD4DC93F8608B4509534CB6961D27C/", None,
+                """
+                Good Awakening Combinations
+                - **PRIMETIME and STINGER**
+                Rasmus' PRIMARY deals an insane amount of damage by applying STINGER to enemy
+
+                - **PRIMETIME and RAPID FIRE**
+                Primary has 2 charges and lower cooldown, good for chipping enemy health
+
+                - **SPECIALIZED TRAINING and EXTRA SPECIAL**
+                Rasmus' SPECIAL will be more powerful and lower the cooldown
+                """,
+                None
+                ]
+    elif p_message == "octavia":
+        return ["https://steamuserimages-a.akamaihd.net/ugc/2019347876656145018/2F8483354B43F0453E6FDBB72F11DBBDFD42C149/", None,
+                """
+                Good Awakening Combinations
+                - **CAST TO LAST and CHRONOBOOST**
+                The duration buff for the secondary ability will last longer, which means you will have a larger time window to refresh it and receive more movement speed.
+
+                - **PRIMETIME and RAPID FIRE**
+                Primary has 2 charges and lower cooldown, good for refreshing the secondary buff duration
+                """,
+                None]
+    elif p_message == 'vyze':
+        return ["https://steamuserimages-a.akamaihd.net/ugc/2019347876656145023/4C70742479722CD6F446E3EFED6FC7F565429F0F/", None,
+                """
+                Good Awakening Combinations
+                - **STINGER and PRIMETIME**
+                Vyce's Primary has 2 charges and can deal an insane amount of damage combined with STINGER.
+
+                - **PRIMETIME and HEAVY IMPACT**
+                Vyce's Primary has 2 charges and lowered cooldown whenever it successfully hits 2 units, making it highly effective for dealing damage to enemies and controlling the core.
+                """,
+                None
+                ]
+    else:
+        return [None, None, None, None]
